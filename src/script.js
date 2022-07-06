6f17db95a23778542ba9ce34a679f1b4c925f2ed
Bertijn Pauwels <bertijn.pauwels@acolad.com>
2022-07-06 14:11:58 +0200

    New look of the website

--- src/script.js
+++ src/script.js
@@ -6,16 +6,21 @@ import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
 import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
 import { GUI } from "lil-gui";
 
-// Cursor
-const cursor = {
-    x: 0,
-    y: 0,
+/**
+ * Debug
+ */
+const gui = new GUI().hide();
+
+const parameters = {
+    materialColor: '#ffeded',
 };
 
-window.addEventListener("mousemove", (event) => {
-    cursor.x = event.clientX / sizes.width - 0.5;
-    cursor.y = -(event.clientY / sizes.height - 0.5);
-});
+gui
+    .addColor(parameters, 'materialColor')
+    .onChange(() => {
+        material.color.set(parameters.materialColor);
+        particlesMaterial.color.set(parameters.materialColor);
+    });
 
 /**
  * Base
@@ -26,97 +31,88 @@ const canvas = document.querySelector("canvas.webgl");
 // Scene
 const scene = new THREE.Scene();
 
-// Fog
-const fog = new THREE.Fog("#18082b", 1, 15);
-scene.fog = fog;
-
 /**
- * Textures
+ * Objects
  */
+// Texture
 const textureLoader = new THREE.TextureLoader();
-const matcapTexture = textureLoader.load("textures/matcaps/4.png");
-const particleTexture = textureLoader.load("textures/particles/4.png");
+const gradientTexture = textureLoader.load('textures/gradients/3.jpg');
+gradientTexture.magFilter = THREE.NearestFilter;
+const particlesTexture = textureLoader.load('textures/particles/3.png');
+
+// Material
+const material = new THREE.MeshToonMaterial({
+    color: parameters.materialColor,
+    gradientMap: gradientTexture,
+});
+
+// Meshes
+const objectsDistance = 4;
+
+const mesh1 = new THREE.Mesh(
+    new THREE.TorusGeometry(1, 0.4, 16, 60),
+    material,
+);
+
+const mesh2 = new THREE.Mesh(
+    new THREE.ConeGeometry(1, 2, 32),
+    material,
+);
+
+const mesh3 = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+    material,
+);
+
+mesh1.position.x = 2;
+mesh2.position.x = -2;
+mesh3.position.x = 2;
+
+mesh1.position.y = - objectsDistance * 0;
+mesh2.position.y = - objectsDistance * 1;
+mesh3.position.y = - objectsDistance * 2;
+
+scene.add(mesh1, mesh2, mesh3);
+
+const sectionMeshes = [mesh1, mesh2, mesh3];
 
 /**
  * Particles
  */
 // Geometry
-const particlesGeometry = new THREE.BufferGeometry()
-const count = 5000
+const particlesCount = 200;
+const positions = new Float32Array(particlesCount * 3);
 
-const positions = new Float32Array(count * 3)
-const colors = new Float32Array(count * 3)
-
-for (let i = 0; i < count * 3; i++) {
-    positions[i] = (Math.random() - 0.5) * 15
-    colors[i] = Math.random()
+for (let i = 0; i < particlesCount; i++) {
+    positions[i * 3 + 0] = (Math.random() - 0.5) * 10;
+    positions[i * 3 + 1] = objectsDistance * 0.5 - Math.random() * objectsDistance * sectionMeshes.length;
+    positions[i * 3 + 2] = (Math.random() - 0.5) * 10;
 }
 
-particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+const particlesGeometry = new THREE.BufferGeometry();
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
 
 // Material
 const particlesMaterial = new THREE.PointsMaterial({
-    color: '#ffffff',
-    // vertexColors: true,
+    color: parameters.materialColor,
     transparent: true,
-    alphaMap: particleTexture,
-    // alphaTest: 0.001,
-    // depthTest: false,
+    alphaMap: particlesTexture,
+    sizeAttenuation: true,
+    size: 0.05,
     depthWrite: false,
     blending: THREE.AdditiveBlending,
-    size: 0.1,
-    sizeAttenuation: true
-})
+});
 
 // Points
-const particles = new THREE.Points(particlesGeometry, particlesMaterial)
-scene.add(particles)
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
 
 /**
- * Fonts
+ * Lights
  */
-const fontLoader = new FontLoader();
-const fontGroup = new THREE.Group();
-scene.add(fontGroup);
-
-fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
-    // Material
-    const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
-
-    // Text
-    const textGeometry = new TextGeometry("Bertijn Pauwels", {
-        font: font,
-        size: 0.5,
-        height: 0.2,
-        curveSegments: 12,
-        bevelEnabled: true,
-        bevelThickness: 0.03,
-        bevelSize: 0.02,
-        bevelOffset: 0,
-        bevelSegments: 5,
-    });
-    textGeometry.center();
-
-    const workInProgressGeometry = new TextGeometry("WORK IN PROGRESS", {
-        font: font,
-        size: 0.6,
-        height: 0.3,
-        curveSegments: 12,
-        bevelEnabled: true,
-        bevelThickness: 0.03,
-        bevelSize: 0.02,
-        bevelOffset: 0,
-        bevelSegments: 5,
-    });
-    workInProgressGeometry.center();
-
-    const text = new THREE.Mesh(textGeometry, material);
-    text.position.y = 0.5;
-    const workInProgressText = new THREE.Mesh(workInProgressGeometry, material);
-    workInProgressText.position.y = -1;
-
-    fontGroup.add(text, workInProgressText);
-});
+const directionalLight = new THREE.DirectionalLight('#ffffff', 1);
+directionalLight.position.set(1, 1, 0);
+scene.add(directionalLight);
 
 /**
  * Sizes
@@ -124,9 +120,9 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight,
-};
+}
 
-window.addEventListener("resize", () => {
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth;
     sizes.height = window.innerHeight;
@@ -143,59 +139,95 @@ window.addEventListener("resize", () => {
 /**
  * Camera
  */
-// Base camera
-const camera = new THREE.PerspectiveCamera(
-    75,
-    sizes.width / sizes.height,
-    0.1,
-    100
-);
-camera.position.x = 0;
-camera.position.y = 0;
-camera.position.z = 15;
-scene.add(camera);
+// Group
+const cameraGroup = new THREE.Group();
+scene.add(cameraGroup);
 
-// Controls
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
-controls.enabled = false;
+// Base camera
+const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100);
+camera.position.z = 6;
+cameraGroup.add(camera);
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
+    alpha: true,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-renderer.setClearColor(new THREE.Color("#18082b"))
+
+/**
+ * Scroll
+ */
+let scrollY = window.scrollY;
+let currentSection = 0;
+
+window.addEventListener('scroll', () => {
+    scrollY = window.scrollY;
+    const newSection = Math.round(scrollY / sizes.height);
+
+    if (currentSection !== newSection) {
+        currentSection = newSection;
+
+        gsap.to(
+            sectionMeshes[currentSection].rotation,
+            {
+                duration: 1.5,
+                ease: 'power2.inOut',
+                x: '+=6',
+                y: '+=3',
+                z: '+=1.5',
+            },
+        );
+    }
+});
+
+
+/**
+ * Cursor
+ */
+const cursor = {
+    x: 0,
+    y: 0,
+}
+
+window.addEventListener('mousemove', (_event) => {
+    cursor.x = _event.clientX / sizes.width - 0.5;
+    cursor.y = _event.clientY / sizes.height - 0.5;
+});
 
 /**
  * Animate
  */
-gsap.to(camera.position, { duration: 2, z: 5 });
-
 const clock = new THREE.Clock();
+let previousTime = 0;
 
 const tick = () => {
     const elapsedTime = clock.getElapsedTime();
+    const deltaTime = elapsedTime - previousTime;
+    previousTime = elapsedTime;
 
-    // Update controls
-    controls.update();
+    // Animate camera
+    camera.position.y = - scrollY / sizes.height * objectsDistance;
 
-    // Update particles
-    particles.rotation.y = elapsedTime * 0.05;
+    const parallaxX = cursor.x * 0.5;
+    const parallaxY = - cursor.y * 0.5;
+    cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime;
+    cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime;
 
-    // Update camera
-    camera.position.x = cursor.x * 3;
-    camera.position.y = cursor.y * 3;
-    camera.lookAt(fontGroup.position);
+    // Animate meshes
+    for (const mesh of sectionMeshes) {
+        mesh.rotation.x += deltaTime * 0.1;
+        mesh.rotation.y += deltaTime * 0.12;
+    }
 
     // Render
     renderer.render(scene, camera);
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick);
-};
+}
 
 tick();
